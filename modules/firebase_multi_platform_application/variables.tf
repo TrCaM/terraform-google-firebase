24306a7b044b1c413e8dff2d1d70ac2301697056
Tri Cao <caot@google.com>
2026-02-25 17:38:11 -0500

    Fix null attribute evaluation in variables.tf and bump version to 12.14.1
    Change-Id: I6b2693f15442b228b0de5465fa88001f2da34222

--- modules/firebase_multi_platform_application/variables.tf
+++ modules/firebase_multi_platform_application/variables.tf
@@ -46,17 +46,17 @@ variable "apps" {
   default = {}
 
   validation {
-    condition     = var.apps.web_app == null || coalesce(var.apps.web_app.enable_app_check, false) == false || try(var.apps.web_app.recaptcha_site_key, null) != null
+    condition     = var.apps.web_app == null || try(var.apps.web_app.enable_app_check, false) == false || try(var.apps.web_app.recaptcha_site_key, null) != null
     error_message = "If App Check is enabled for the Web App, 'recaptcha_site_key' must be provided."
   }
 
   validation {
-    condition     = var.apps.android_app == null || coalesce(var.apps.android_app.enable_app_check, false) == false || length(coalesce(var.apps.android_app.sha256_hashes, [])) > 0
+    condition     = var.apps.android_app == null || try(var.apps.android_app.enable_app_check, false) == false || length(try(var.apps.android_app.sha256_hashes, [])) > 0
     error_message = "If App Check is enabled for the Android App, at least one 'sha256_hashes' value must be provided."
   }
 
   validation {
-    condition     = var.apps.apple_app == null || coalesce(var.apps.apple_app.enable_app_check, false) == false || try(var.apps.apple_app.team_id, null) != null
+    condition     = var.apps.apple_app == null || try(var.apps.apple_app.enable_app_check, false) == false || try(var.apps.apple_app.team_id, null) != null
     error_message = "If App Check is enabled for the Apple App, 'team_id' must be provided."
   }
 }
