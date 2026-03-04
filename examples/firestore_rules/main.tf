27a044215b9202bc016203779f8b117ef4c6662f
Lingkai Shen <lingkai@google.com>
2026-03-04 06:15:14 +0000

    Add back project ID to example

--- examples/firestore_rules/main.tf
+++ examples/firestore_rules/main.tf
@@ -31,6 +31,7 @@ resource "google_firestore_database" "database" {
 
 module "firestore_rules" {
   source      = "../../modules/firestore_rules"
+  project_id     = var.project_id
   database_id = google_firestore_database.database.name
   rules_content = <<EOT
 rules_version = '2';
